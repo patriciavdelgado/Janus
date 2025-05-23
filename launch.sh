383d7fac50f0a01aa6d6c0c2c9c7babbfec4ec50
patriciavdelgado <131043808+patriciavdelgado@users.noreply.github.com>
2025-05-23 11:38:58 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min32gb     # Reserved partition
-#SBATCH --qos=gpu_min32gb         # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --partition=gpu_min8gb     # Reserved partition
+#SBATCH --qos=gpu_min8gb         # QoS level. Must match the partition name. External users must add the suffix "_ext".
 #SBATCH --job-name=janus    # Job name
 #SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
