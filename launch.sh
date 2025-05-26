a5568eec8c8c38e19c7cef18f317b8cc0da6f425
patriciavdelgado <131043808+patriciavdelgado@users.noreply.github.com>
2025-05-26 23:01:33 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min24gb     # Reserved partition
-#SBATCH --qos=gpu_min24gb_ext         # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --partition=gpu_min32gb     # Reserved partition
+#SBATCH --qos=gpu_min32gb_ext         # QoS level. Must match the partition name. External users must add the suffix "_ext".
 #SBATCH --job-name=janus    # Job name
 #SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
@@ -10,4 +10,4 @@ echo "Running job in reserved partition"
 
 # Commands / scripts to run (e.g., python3 train.py)
 
-python3 JP1B_SUIM_10iterations.py
+python3 JP7B_SUIM_10iterations.py
